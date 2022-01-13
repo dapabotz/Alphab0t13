6503bf091efa6ce6671368702cef5d68da387b94
Andrii Street Moonshine <93533006+Andriiwalker@users.noreply.github.com>
2022-01-13 14:32:36 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,5 +1,5 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY ANDRIIxBOTZ
 
 *🏧 BANK*
 
@@ -15,13 +15,13 @@ return `PAYMENT BY ZEEONE OFC
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0838 2275 9168
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0838 2275 9168
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0838 2275 9168
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Andrii Street Moonshine
 
 *📲 PULSA*
 
